9779619550771ff707e5bf3640c56bf53800a4a1
Gaudi <goncharov.kostiantyn@gmail.com>
2024-06-02 03:54:51 +0300

    fixed some styles

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,11 +1,11 @@
-import Accordion from 'accordion-js'
+import Accordion from 'accordion-js';
 
 new Accordion('.faq-list', {
   duration: 400,
   showOne: true,
   showMultiple: true,
   onToggle: function (event) {
-    const button = event.target.closest('.qwestions');
+    const button = event.target.closest('.questions');
     if (button) {
       button.classList.toggle('is-active');
     }
